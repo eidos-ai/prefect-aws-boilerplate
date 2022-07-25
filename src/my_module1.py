c34566001c30f712cbc4f7aa70f8f57615b2d9d7
Valeria <valeriax21p@gmail.com>
2022-07-25 15:41:53 -0300

    Commented import load_dotenv. Uncomment if using dotenv file. See README.md.

--- src/my_module1.py
+++ src/my_module1.py
@@ -1,10 +1,10 @@
 import prefect, os
-from dotenv import load_dotenv 
+# from dotenv import load_dotenv 
 
-load_dotenv() 
-my_env_var = os.getenv("MY_ENV_VARIABLE")
+# load_dotenv() 
+# my_env_var = os.getenv("MY_ENV_VARIABLE")
 
 def my_function1():
     logger = prefect.context.get("logger")
     logger.info("This is test function #1")
-    logger.info(f"This is my environment variable: {my_env_var}")
+    # logger.info(f"This is my environment variable: {my_env_var}")
