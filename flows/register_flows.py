93bacef0ba43fea688eb58111258f53057bb2b04
Valeria <valeriax21p@gmail.com>
2022-07-28 11:42:36 -0300

    print path

--- flows/register_flows.py
+++ flows/register_flows.py
@@ -1,7 +1,7 @@
 import sys
 print(sys.path)
-from flows.my_flow1 import flow as flow1 # rename flow from my_flow1.py as flow1 
-from flows.my_flow2 import flow as flow2 # rename flow from my_flow2.py as flow2 and so on... 
+from my_flow1 import flow as flow1 # rename flow from my_flow1.py as flow1 
+from my_flow2 import flow as flow2 # rename flow from my_flow2.py as flow2 and so on... 
 
 flow1.register(project_name="hello-world") # register flow1
 flow2.register(project_name="hello-world") # register flow2
